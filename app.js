66fe40b4ca264f4188fdf252a55374746429d2c7
Jatin <jatinsharma9038@gmail.com>
2022-09-08 23:48:57 +0530

    New Features added check Update.txt

--- app.js
+++ app.js
@@ -3,21 +3,46 @@ console.log(a);
 let btn = document.getElementById("add");
 let box = document.getElementById("cont");
 
-btn.addEventListener("click", (e) => {
-  console.log(e.target.files);
-  let newRecord = document.createElement("div");
-  newRecord.innerHTML = `<img src="imgs/user1.png" alt="" width="50px" height="60px" />
-          <div id="text">
-            <p>Name: <span>${a[0].value}</span></p>
-            <p>
-              About:
-              <span
-                >${a[2].value}</span
-              >
-            </p>
-          </div>`;
-  newRecord.classList.add("record");
-  box.appendChild(newRecord);
+btn.addEventListener("click", () => {
+  let count = 0;
+  if (a[0].value == "") {
+    count = 1;
+    a[0].style.borderColor = "red";
+  }
+  if (a[1].value == "") {
+    count = 1;
+    a[1].style.borderColor = "red";
+  }
+  if (a[2].value == "") {
+    count = 1;
+    a[2].style.borderColor = "red";
+  }
+  if (a[2].value.length > 212) {
+    a[2].value = a[2].value.slice(1, 210);
+    a[2].value = a[2].value.concat("...");
+  }
+  // console.log(e.target.files);
+  if (count == 0) {
+    a[0].style.borderColor = "#727070";
+    a[1].style.borderColor = "#727070";
+    a[2].style.borderColor = "#727070";
+    let newRecord = document.createElement("div");
+    newRecord.innerHTML = `<img src="${a[1].value}" alt="User Image" width="50px" height="60px" />
+    <div id="text">
+    <p>Name: <span>${a[0].value}</span></p>
+    <p>
+    About:
+    <span
+    >${a[2].value}</span
+    >
+    </p>
+    </div>`;
+    newRecord.classList.add("record");
+    box.appendChild(newRecord);
+    a[0].value = "";
+    a[1].value = "";
+    a[2].value = "";
+  }
 });
 
 let body = document.getElementById("body");
